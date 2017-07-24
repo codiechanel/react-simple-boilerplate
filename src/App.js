2ea9831b3203c143116ce6525e4e95a9b19d7acf
codie <codiechanel@gmail.com>
2017-07-24 14:28:49 +0800

    nicd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -11,7 +11,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+  
           <h2>Welcome to  React</h2>
         </div>
         <p className="App-intro">
