bd319d272c298af557b313324136314ef5cb724c
codie <codiechanel@gmail.com>
2017-07-24 14:32:11 +0800

    ye

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     console.log('mount')
   }
-  
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-  
-          <h2>Welcome to  React</h2>
+          <h2>Welcome to cool React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
